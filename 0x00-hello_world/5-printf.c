57f51f861a2f6ad7b804e5efa3de0edb9dba4735
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-07 15:11:13 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
-/** main - This is the main function
+/**
+ * main - This is the main function
  *
- *  Return - This signifies the end of our code
+ * Return - This signifies the end of our code
  */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
