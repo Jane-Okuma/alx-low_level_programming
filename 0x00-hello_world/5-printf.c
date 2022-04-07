ad84c32f34436f2d90596be246e284316dc67aac
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-07 14:51:22 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/** main - This is the main function
+ *
+ *  Return - This signifies the end of our code
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+}
