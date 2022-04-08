9d71c24baa79ae997953d343e852d383760f3fd8
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-08 15:14:41 -0700

    Blehh

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - This is entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a[];
+
+	int b[];
+
+	int i;
+
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		a[i] = i;
+
+		b[i] = i;
+	}
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (a[i] < b[j] && a[i] != b[j])
+			{
+				putchar(a[i] + '0');
+				putchar(b[j] + '0');
+				if (a[i] != 8 && b[j] != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
