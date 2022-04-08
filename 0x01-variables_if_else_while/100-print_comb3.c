5dc283bc4d7071bdf9d04c15de7c4ea7ac66f065
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-08 15:24:18 -0700

    blehh

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -28,11 +28,11 @@ int main(void)
 			{
 				putchar(a[i] + '0');
 				putchar(b[j] + '0');
-			}
-			if (a[i] != 8 && b[j] != 9)
-			{
-				putchar(',');
-				putchar(' ');
+				if (a[i] != 8 && b[j] != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
