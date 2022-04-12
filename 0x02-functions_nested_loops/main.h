42fa5f3bc9fff350585e3a89c15091e7a6aa2ae9
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-12 14:22:40 -0700

    A

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,13 @@
+// Print alphabes in lower case
+void print_alphabet(void)
+{
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
 
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alpha[i];
+	}
+	putchar('\n');
+}
