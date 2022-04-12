2a30de16a5b75a98c7c21d2645b69aa179a6dc4b
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-12 15:59:50 -0700

    e

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Returns the absolute value of an integer
+ *
+ * Return: Always 0
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (-1 * a);
+	}
+}
