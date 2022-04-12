c239a015fa8ce3fccf5e5cdf7395b48646150817
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-12 16:23:31 -0700

    f

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints the last digit of a given integer
+ *
+ * @a : integer number
+ * Return: Always 0
+ */
+int print_last_digit(int a)
+{
+	return (a % 10);
+}
