c06cd28ad1021ff0f46a236dbc80c2b06d4ddd49
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-12 19:37:25 -0700

    op

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,26 @@ void times_table(void)
 {
 	int i, j;
 	
-	for (i = 0; i < 10; i++)
+	_putchar('0');
+	for (i = 1; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			int a = i * j;
-			_putchar('0' + a);
+
+			_putchar(',');
+			_putchar(' ');
+			if (a <= 9)
+			{
+				_putchar(' ');
+				_putchar('0' + a);
+			}
+			else
+			{
+				_putchar((a / 10) + 48);
+				_putchar((a % 10) + 48);
+			}
+			_putchar('\n');
 		}
 	}
 }
