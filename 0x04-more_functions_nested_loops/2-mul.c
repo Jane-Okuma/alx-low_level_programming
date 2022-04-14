10f1c9c323a1b0db0a861a033222ebe88a873bf4
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-14 11:29:25 -0700

    Comm3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - Multiply two integers
+ *
+ * @a : integer 1
+ * @b : integer 2
+ * Return: returns product of a and b
+ */
+int mul(int a, int b)
+{
+	int i;
+
+	i = a * b;
+	return (i);
+}
