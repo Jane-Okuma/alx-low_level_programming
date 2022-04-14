3d79459b782a6c92bf6a73b312c1b716c9f9716c
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-14 14:46:28 -0700

    Comm10

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Contains code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
+		else if (i % 3 == 0 && !(i % 5 == 0))
+		{
+			printf("Fizz");
+			printf(" ");
+		}
+		else if (!(i % 3 == 0) && i % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%d", i);
+			printf(" ");
+		}
+	}
+}
