bd52a656b91755e19f10a74648bed5da5ec74d26
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-20 13:57:07 -0700

    Comm5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * print_rev - Prints the reverse of a string
+ *
+ * @s : points to string s
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i = strlen(s);
+
+	while (i - 1 <= 0)
+	{
+		int j = i - 1;
+
+		putchar(*(s + j));
+	}
+	putchar('\n');
+}
