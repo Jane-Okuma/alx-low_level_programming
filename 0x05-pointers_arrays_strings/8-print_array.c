33f9344a131d7869dc2786b0e39e2342bbf6efb1
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-20 16:45:06 -0700

    COmm9

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_array - prints array
+ *
+ * @a : pointer
+ * @n : number of elements to be printed
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0; b < n; b++)
+	{
+		printf("%d", a[b]);
+		if (b < n - 1)
+		{
+			printf(",");
+		}
+	}
+}
