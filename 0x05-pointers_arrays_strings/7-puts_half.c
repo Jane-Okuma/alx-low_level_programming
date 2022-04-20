061e35925a5a12287237766d9ccfa5de5fb58a10
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-20 16:21:43 -0700

    Comm8

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts_half - prints the second half of a string
+ *
+ * @str : pointer
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+	int i = strlen(str);
+
+	i = i - 1;
+
+	int j;
+
+	if (i % 2 != 0)
+	{
+		j = (i - 1) / 2;
+	}
+	else
+	{
+		j = i / 2;
+	}
+	while (j <= i)
+	{
+		putchar(*(s + j));
+		j++;
+	}
+	putchar('\n');
+}
