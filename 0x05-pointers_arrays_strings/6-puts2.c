1c79e8d12b0a034c434fd1a604bef7f01225be6a
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-20 16:03:47 -0700

    Comm7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,19 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 #include <string.h>
 /**
  * puts2 - idk
  *
- * @str : idk
- * Return: nothing
+ * @str : pointer
+ * Return: 0
  */
 void puts2(char *str)
 {
+	int size = strlen(str);
+
 	int i = 0;
 
-	while(*str != '\0')
+	while (i <= size)
 	{
 		putchar(*(str + i));
 		i = i + 2;
