0c7f86396aff63e25b8454d54cf1ae5ce72e281e
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-23 10:37:53 -0700

    Comm1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,21 +10,31 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int dest_size = strlen(dest);
+	int dest_size = 0;
 
-	int src_size = strlen(src);
+	int src_size = 0;
 
+	while (*dest != '\0')
+	{
+		dest_size++;
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		src_size++;
+		src++;
+	}
 	int i;
 
 	int j = 0;
 
 	for (i = dest_size - 1; i < (dest_size - 1) + src_size; i++)
 	{
-		if (j < size)
+		if (j < src_size)
 		{
 			*(dest + i) = *(src + j);
 		}
-		j++
+		j++;
 	}
 	return (*dest);
 }
