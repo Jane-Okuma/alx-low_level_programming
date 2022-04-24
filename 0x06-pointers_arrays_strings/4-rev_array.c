b85f2d65532059bf69cf2ca6fecb36c40e287816
Jane-Okuma <101204451+Jane-Okuma@users.noreply.github.com>
2022-04-25 00:36:19 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,19 +7,15 @@
  */
 void reverse_array(int *a, int n)
 {
-	int b[n];
+	int i, j, temp;
 
-	int i;
-
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n - 1; i++)
 	{
-		b[i] = *(a + i);
-	}
-	int j = 0;
-
-	for (i = n - 1; i >= 0; i--)
-	{
-		*(a + j) = b[i];
-		j++;
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
 	}
 }
