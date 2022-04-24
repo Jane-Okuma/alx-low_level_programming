efcf825f1b4c20cccd901469ccd8d13366d349d9
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-24 04:53:40 -0700

    Comm3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i > n)
+	while (*(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i);
-		if (*(src + i) == '\0')
-			break;
 		i++;
 	}
 	return (dest);
