2f0dda11623fd8cfbf5273488786c35b88db7231
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-04-27 11:41:38 -0700

    Comm5

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,28 +1,3 @@
 #include "main.h"
-/*
- * _strspn - soes something
- * @s - string that is searched through
- * @accept - search substring
- * Return: Length
- */
-unsigned int _strspn(char *s, char *accept)
-{
-	int i;
-
-	int j;
-
-	unsigned int ln = 0;
-
-	for (i = 0; *(accept + i) != '\0'; i++)
-	{
-		for (j = 0; *(s + j) != '\0'; j++)
-		{
-			if (*(s + j) == *(accept + i))
-			{
-				ln++;
-				break;
-			}
-		}
-	}
-	return (ln);
-}
+/**
+ * 
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strpbrk - does something
+ * @s: pointer
+ * @accept: pointer
+ * Return: char.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	int j;
+
+	int ln = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; i++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				ln = 1;
+				break;
+			}
+		}
+		if (ln = 1)
+			break;
+	}
+	if (ln = 1)
+		return (s + i);
+	if (ln - 0)
+		return ('\0');
+}
