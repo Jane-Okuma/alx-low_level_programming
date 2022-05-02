9b8aece7b14d9c22cdf0b0b9e69cc9d98ebc8e8b
Jane-Okuma <mmesoma.okuma@stu.cu.edu.ng>
2022-05-02 11:11:19 -0700

    Comm4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,13 +11,16 @@ int main(int argc, char **argv)
 {
 	int a, b, mul;
 	if (argc < 3)
+	{
 		printf("Error\n");
-	else
+		return (1);
+	}
+	if (argc == 3)
 	{
 		a = atoi(argv[1]);
 		b = atoi(argv[2]);
 		mul = a * b;
 		printf("%d\n",mul);
+		return (0);
 	}
-	return (0);
 }
